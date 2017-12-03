7d42777d91237945b44ad2d87c397ba896b84f48
programigos <programigos5@gmail.com>
2017-12-03 10:38:30 -0500

    Add files via upload

--- leer_txt.cpp
+++ leer_txt.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <locale>
 
 using namespace std;
 
@@ -14,17 +15,19 @@ public:
     string posicion;
     string equipo;
     void imprimir(){
-        cout << camiseta << " " << apellido << " " << posicion << " " << edad << endl;
+        cout << camiseta << " " << apellido << " " << posicion << " " << edad << " " << equipo << endl;
     }
 };
 
-int main(){                             //Podemos meter todo esto en una funcion de crear jugadores
-    ifstream entrada;         
+int main(){
+    setlocale(LC_ALL,"spanish");
+    ifstream entrada;
     vector<jugador> players;
     string num;
     string ape;
     string pos;
     string ed;
+    string equi;
     string completo;
     jugador hola;
     entrada.open("teams.txt");
@@ -34,14 +37,17 @@ int main(){                             //Podemos meter todo esto en una funcion
         ape = completo.substr(completo.find('-')+1);
         pos = ape.substr(ape.find('-')+1);
         ed = pos.substr(pos.find('-')+1);
+        equi = ed.substr(ed.find('-')+1);
 
         ape = ape.substr(0,ape.find('-'));
         pos = pos.substr(0,pos.find('-'));
+        ed = ed.substr(0,pos.find('-'));
 
         hola.apellido = ape;
         hola.camiseta = atoi(num.c_str());
         hola.edad = atoi(ed.c_str());
         hola.posicion = pos;
+        hola.equipo = equi;
         players.push_back(hola);
     }
     for(int i = 0; i < players.size(); i++){
