659764f61361976a3a09f235132916ed4940e78c
programigos <programigos5@gmail.com>
2017-12-02 21:43:09 -0500

    el .zpr es un proyecto de zinjaI

---
+++ gusgus/MAIN.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include "aveliano.h"
+#include "lista.h"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void sleep(){
+	int i=0,o;
+	while(i<100000){
+		o=1+1+1+1+1+1+1+1+1+1+1+1;
+		o=0;
+		i++;
+	}
+}
+
+struct Player{
+	unsigned int dorsal;
+	string apellido;
+	unsigned int edad;
+	string seleccion;
+	char posicion;
+	
+	Player(unsigned int nro,string ap,unsigned int ed,string sl,char pos){
+		dorsal=nro;
+		apellido=ap;
+		edad=ed;
+		seleccion=sl;
+		posicion=pos;
+	}
+	~Player(){}
+};
+void insertarSeleccion(string txt,vector<list<Player*>> &paises){
+	list<Player*> nn(txt);
+	cout<<nn.name<<endl;
+	paises.push_back(nn);
+}
+void insertarJugador(vector<list<Player*>> &paises){
+	string n,ap;
+	char pos;
+	unsigned int no,ed;
+	cout<<"Nacionalidad    : ";cin>>n;
+	cout<<"Apellido        : ";cin>>ap;
+	cout<<"Edad            : ";cin>>ed;
+	cout<<"Posicion        : ";cin>>pos;
+	cout<<"Nro de camiseta : ";cin>>no;
+	Player *newPlayer = new Player(no,ap,ed,n,pos);
+	paises[0].insert(newPlayer);
+//	cout<<paises[0][0]->apellido<<"sssws"<<endl;
+}
+void eliminarSeleccion(){}
+void eliminarJugador(){}
+
+int main(){
+	vector<list<Player*>> Naciones;
+	list<Player*> pe("Peru");
+	Naciones.push_back(pe);
+	AvelianTree<list<Player*>,Mayor<int>> porEdad , porPos , RUSSIA;
+	
+	///first menu
+	cout<<"  ______________________________________  "<<endl;
+	cout<<"||--------------------------------------||"<<endl;
+	cout<<"||        - - - Russia 2018 - - -       ||"<<endl;
+	cout<<"||______________________________________||"<<endl;
+	cout<<"  --------------------------------------  "<<endl;
+	
+	char opcion;
+	while(true){
+		cout<<"  --------------------------------------  "<<endl;
+		cout<<"0 -> Salir"<<endl;
+		cout<<"1 -> Modificar"<<endl;
+		cout<<"2 -> Visualizar"<<endl;
+		cout<<endl;
+		cout<<"Ingrese una opcion:"<<endl;
+		cin>>opcion;
+		system("cls");
+		if(opcion == '0'){
+			cout<<"-CERRADO-"<<endl;
+			break;
+		}
+		else if(opcion == '1'){
+			cout<<"--------------MODIFICAR---------------"<<endl;
+			cout<<"1 -> añadir seleccion"<<endl;
+			cout<<"2 -> añadir jugador"<<endl;
+			cout<<"3 -> eliminar seleccion"<<endl;
+			cout<<"4 -> eliminar jugador"<<endl;
+			cout<<endl;
+			cout<<"0 -> atras"<<endl;
+			cin>>opcion;
+			system("cls");
+			if(opcion=='1'||opcion=='2'||opcion=='3'||opcion=='4'){
+				switch(opcion){
+				case '1':
+					insertarSeleccion("mexico",Naciones);
+					break;//insertar seleccion
+				case '2':
+					insertarJugador(Naciones);
+					break;//insertar jugador
+				case '3':
+					break;//borrar seleccion
+				case '4':
+					break;//borrar jugador
+				}
+			}
+			else{
+				;
+			}
+		}
+		else if(opcion == '2'){
+			cout<<"-------------VISUALIZAR--------------"<<endl;
+			cout<<"1 -> Listado por Edad"<<endl;
+			cout<<"2 -> Listado por Posicion"<<endl;
+			cout<<"3 -> Listado por Selecciones"<<endl;
+			cout<<endl;
+			cout<<"0 -> atras"<<endl;
+			cin>>opcion;
+			system("cls");
+			if(opcion=='1'||opcion=='2'||opcion=='3'){
+				switch(opcion){
+				case '1':
+					break;//lista por edad
+				case '2':
+					break;//lista por posicion
+				case '3':
+					break;//lista de selecciones
+				}
+			}
+			else{
+				;
+			}
+		}
+		system("cls");
+	}
+	return 0;
+}
---
+++ gusgus/lista.h
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+using namespace std;
+template <class T >
+struct nodo {
+	T valor;
+	nodo<T>* next=nullptr;
+	
+	nodo();
+	nodo(T v, nodo<T>* next);
+	~nodo();
+};
+template <class T>
+nodo<T>::nodo(){}
+
+template <class T>
+nodo<T>::nodo(T v,nodo<T>* n_next)
+{
+	this->valor = v;
+	this->next = n_next;
+}
+template <class T>
+nodo<T>::~nodo() {}
+
+template <class T>
+struct list {
+	nodo<T>* head;
+	string name;
+	list(string);
+	~list();
+	bool find(T x, nodo<T>**& p);
+	bool insert(T x);
+	bool erase(T x);
+	void print(void);
+	T operator [](unsigned int i){
+		nodo<T> *t = head;
+		unsigned int x=0;
+		while (t) {
+			if(x==i)
+				return t->valor;
+			t = t->next;
+		}
+	}
+};
+template <class T>
+list<T>::list(string a)
+{
+	head = nullptr;
+	name = a;
+}
+template <class T>
+list<T>::~list()
+{
+	nodo<T>*temp = head;
+	while (head->next)
+	{
+		head = head->next;
+		delete temp;
+		temp = head;		
+	}
+}
+template <class T>
+bool list<T>::find(T x, nodo<T>**&p)
+{
+	p = &head;
+	while (*p && (*p)->valor < x)
+		p = &((*p)->next);
+	return (*p) && (*p)->valor == x;
+}
+template <class T>
+bool list<T>::insert(T x)
+{
+	nodo<T>** p;
+	if (find(x, p)) return 0;
+	*p = new nodo<T>(x,*p);
+	/*nodo<T>* t = new nodo<T>(x);
+	t->next = *p;
+	*p = t;*/
+	return 1;
+}
+template <class T>
+bool list<T>::erase(T x)
+{
+	nodo<T>**p;
+	if (!find(x, p)) return 0;
+	nodo<T>* t = *p;
+	*p = (*p)->next;
+	delete t;
+	return 1;
+}
+template <class T>
+void list<T>::print(void)
+{
+	cout << "- ";
+	nodo<T> *t = head;
+	while (t) {
+		cout << t->valor << " - ";
+		t = t->next;
+	}
+	cout << endl;
+}
+//-----------------------------------------------------------//
