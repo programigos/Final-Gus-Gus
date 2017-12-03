904ebdb8e0f4a38e1520b994bc8d7edd793d6ba8
programigos <programigos5@gmail.com>
2017-12-03 01:00:34 -0500

    Lectura de txt
    Falta solo modificar para que lea equipos y los inserte

---
+++ leer_txt.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class jugador{
+public:
+    int camiseta;
+    int edad;
+    string apellido;
+    string posicion;
+    string equipo;
+    void imprimir(){
+        cout << camiseta << " " << apellido << " " << posicion << " " << edad << endl;
+    }
+};
+
+int main(){
+    ifstream entrada;
+    vector<jugador> players;
+    string num;
+    string ape;
+    string pos;
+    string ed;
+    string completo;
+    jugador hola;
+    entrada.open("teams.txt");
+    while(!entrada.eof()){
+        getline(entrada,completo);
+        num = completo.substr(0,completo.find('-'));
+        ape = completo.substr(completo.find('-')+1);
+        pos = ape.substr(ape.find('-')+1);
+        ed = pos.substr(pos.find('-')+1);
+
+        ape = ape.substr(0,ape.find('-'));
+        pos = pos.substr(0,pos.find('-'));
+
+        hola.apellido = ape;
+        hola.camiseta = atoi(num.c_str());
+        hola.edad = atoi(ed.c_str());
+        hola.posicion = pos;
+        players.push_back(hola);
+    }
+    for(int i = 0; i < players.size(); i++){
+        players[i].imprimir();
+    }
+    entrada.close();
+    return 0;
+}
